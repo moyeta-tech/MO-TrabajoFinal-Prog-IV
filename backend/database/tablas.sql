a834feb8710bd7b0e141cddd42863b7be2d42ca2
moyeta-tech <octaviomoya05@gmail.com>
2025-11-03 20:11:30 -0300

    Agregando archivo .sql con las tablas del proyecto

---
+++ backend/database/tablas.sql
@@ -0,0 +1,45 @@
+-- TABLA USUARIOS
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `hash_contraseña` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- TABLA PACIENTES
+CREATE TABLE `pacientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `dni` int NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `obra_social` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- TABLA MEDICOS
+CREATE TABLE `medicos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `especialidad` varchar(50) NOT NULL,
+  `matricula_profesional` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+-- TABLA TURNOS
+CREATE TABLE `turnos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `paciente_id` int NOT NULL,
+  `medico_id` int NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` varchar(45) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `observaciones` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_turnos_paciente_id_idx` (`paciente_id`),
+  KEY `fk_turnos_medico_id_idx` (`medico_id`),
+  CONSTRAINT `fk_turnos_medico_id` FOREIGN KEY (`medico_id`) REFERENCES `medicos` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_turnos_paciente_id` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
